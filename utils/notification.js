f9a60a4560065d08e0b5bb576e3e64fed205c6db
alexander.beletsky <alexander.beletsky@gmail.com>
2013-04-04 16:53:36 +0300

    added all developers to notification list

--- utils/notification.js
+++ utils/notification.js
@@ -1,12 +1,18 @@
 var mandrill = require('node-mandrill')(process.env.MANDRILL_TOKEN);
 
 function sendEmail(text, callback) {
+	var developers = [
+		{email: 'alexander.beletsky@gmail.com'},
+		{email: 'anton.mamant@gmail.com'},
+		{email: 'dmitri.voronianski@gmail.com'}
+	];
+
 	return mandrill('/messages/send', {
 		message: {
 			text: text,
 			from_email: 'app@likeastore.com',
 			subject: 'New user waiting for release!',
-			to: [ {email: 'alexander.beletsky@gmail.com'}]
+			to: developers
 		}
 	}, callback);
 }
