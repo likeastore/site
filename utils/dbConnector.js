93e14b25c9bf3876c388f2da474410427f657f30
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-04-06 21:19:18 +0300

    additional

--- utils/dbConnector.js
+++ utils/dbConnector.js
@@ -1,6 +1,7 @@
 /**
  * Mongodb connector
  */
+var url = require('url');
 var config = require('../config')();
 var mongo = require('mongojs');
 
@@ -10,9 +11,15 @@ var collections = ['subscribers'];
 // get db with default collections list
 var db = mongo.connect(config.connection, collections);
 
-// overwrite collections if needed
-var setDb = function (collections) {
-	return mongo.connect(config.connection, [collections]);
+/* 
+ * Overwrite db url or collections if needed
+ * @param options {Object} - possible values:
+ * url {String}, collection {Array} 
+ * e.g. { url: username:password@host:port/dbname?params), collections: ['name', 'name2' ...] }
+ */
+var setDb = function (options) {
+	var url = options.url ? options.url : config.connection;
+	return mongo.connect(url, options.collections);
 };
 
 module.exports = {
