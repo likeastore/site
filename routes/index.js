886e1633addbb9a19dc7b38fd38ac8baa22f7f10
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-04-07 15:46:40 +0300

    remove authorization connected stuff

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,13 @@
 var subscribers = require('./subscribersFactory');
 
 var index = function (req, res) {
-	/*if (req.session.auth) {
-		res.render('master');
-	} else {*/
-		res.render('index');
-	//}
+	res.render('index');
 };
 
 var notify = function (req, res) {
 	subscribers.save(req, function (err, saved) {
 		if (err) {
-			res.send(500);
+			return res.send(500);
 		}
 		res.send(200);
 	});
