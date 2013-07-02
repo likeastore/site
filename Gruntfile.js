a2075f8ba4dec7506034d80a0977438ed30dd63c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-07-03 00:23:45 +0300

    add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,14 @@ module.exports = function (grunt) {
 					'public/build/styles.min.css': ['public/css/main.css']
 				}
 			}
+		},
+
+		jshint: {
+			js: ['public/js/**/*.js', 'source/**/*.js']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.registerTask('default', ['cssmin']);
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.registerTask('default', ['cssmin', 'jshint']);
 };
