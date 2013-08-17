e23478743747dc2b64e1dc74fa953356e7136e17
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-08-17 14:28:18 +0300

    tabs to app.js

--- app.js
+++ app.js
@@ -11,52 +11,52 @@ authorize.init(passport);
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.engine('ejs', engine);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'likeastore_marketing' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(authorize.localUserSession);
-  app.use(app.router);
+	app.set('port', process.env.PORT || 3000);
+	app.engine('ejs', engine);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.session({ secret: 'likeastore_marketing' }));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(authorize.localUserSession);
+	app.use(app.router);
 });
 
 app.configure('development', function() {
-  app.use(express.logger('dev'));
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.errorHandler());
+	app.use(express.logger('dev'));
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(express.errorHandler());
 });
 
 app.configure('staging', function () {
-  console.log(config.access.user + ' ' + config.access.password);
-  app.use(express.basicAuth(config.access.user, config.access.password));
-  app.use(express.logger('short'));
-  app.use(express.compress());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.errorHandler());
+	console.log(config.access.user + ' ' + config.access.password);
+	app.use(express.basicAuth(config.access.user, config.access.password));
+	app.use(express.logger('short'));
+	app.use(express.compress());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(express.errorHandler());
 });
 
 app.configure('production', function() {
-  app.use(express.logger('short'));
-  app.use(express.compress());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.errorHandler());
+	app.use(express.logger('short'));
+	app.use(express.compress());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(express.errorHandler());
 });
 
 app.configure('test', function() {
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.errorHandler());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(express.errorHandler());
 });
 
 require('./source/api.js')(app, passport);
 require('./source/router.js')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 module.exports = app;
