d18bfbab1370cb783350373720a68b888d88c159
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-10-02 22:56:30 +0300

    more safe body parser

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
 	app.use(express.cookieParser());
-	app.use(express.bodyParser());
+	app.use(express.json());
+	app.use(express.urlencoded());
 	app.use(express.methodOverride());
 	app.use(express.session({ secret: 'likeastore_marketing' }));
 	app.use(passport.initialize());
