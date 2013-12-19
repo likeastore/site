bae1969d86110b81bbec7aa8e3e3a9cc1572c0fd
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-19 11:10:19 +0200

    clean db script corrected

--- bin/cleanDb.js
+++ bin/cleanDb.js
@@ -1,17 +1,14 @@
 process.env.NODE_ENV = process.env.NODE_ENV || process.argv[2] || 'development';
 
-var db = require('./../source/db/dbConnector').db;
+var config = require('../config');
+var db = require('../source/db')(config);
 
-var clean = function () {
-	console.log('cleaning up likestore db...');
-	db.dropDatabase();
-	db.close(function (err) {
-		if (err) {
-			return console.log('clean up failed: %s', err);
-		}
+console.log('cleaning up likestore db...');
+db.dropDatabase();
+db.close(function (err) {
+	if (err) {
+		return console.log('clean up failed: %s', err);
+	}
 
-		return console.log('%s db is clean now!', process.env.NODE_ENV);
-	});
-};
-
-clean();
+	return console.log('%s db is clean now!', process.env.NODE_ENV);
+});
