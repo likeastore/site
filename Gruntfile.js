a2002f94f862e9c92bb25aa1c9ab50b9f4305975
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-02-15 15:57:26 +0200

    add base_old, modernizr, grunt watch & myth

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,91 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
+		myth: {
+			all: {
+				files: {
+					'public/css/developers.css': 'myth/developers.css',
+					'public/css/general.css': 'myth/general.css',
+					'public/css/icons.css': 'myth/icons.css'
+				}
+			}
+		},
+
 		cssmin: {
 			combine: {
 				options: {
 					report: 'gzip'
 				},
 				files: {
+					'public/build/base_merge.min.css': ['public/css/merge/base_merge.css'],
+
+					// old ones
 					'public/build/common_merge.min.css': ['public/css/merge/common_merge.css'],
 					'public/build/homepage_merge.min.css': ['public/css/merge/homepage_merge.css'],
 					'public/build/register_merge.min.css': ['public/css/merge/register_merge.css'],
-					'public/build/share_merge.min.css': ['public/css/merge/share_merge.css'],
-					'public/build/geek_landing_merge.min.css': ['public/css/merge/geek_landing_merge.css']
+					'public/build/share_merge.min.css': ['public/css/merge/share_merge.css']
 				}
 			}
 		},
 
 		jshint: {
 			js: ['public/js/**/*.js', 'source/**/*.js']
+		},
+
+		concat: {
+			options: {
+				stripBanners: true
+			},
+			base: {
+				src: [
+					'public/components/modernizr/modernizr.js',
+					'public/components/vue/dist/vue.js'
+				],
+				dest: 'public/build/base_merge.js'
+			},
+			base_jquery: {
+				src: [
+					'public/components/modernizr/modernizr.js',
+					'public/components/jquery/jquery.js',
+					'public/js/jquery.helpers.js',
+					'public/js/formular.js'
+				],
+				dest: 'public/build/base_jq_merge.js'
+			}
+		},
+
+		uglify: {
+			options: {
+				compress: true,
+				report: 'gzip'
+			},
+			target: {
+				files: {
+					'public/build/base_merge.min.js': ['public/build/base_merge.js'],
+					'public/build/base_jq_merge.min.js': ['public/build/base_jq_merge.js'],
+				}
+			}
+		},
+
+		watch: {
+			styles: {
+				files: ['myth/**/*.css'],
+				tasks: ['myth']
+			},
+			scripts: {
+				files: ['public/js/**/*.js'],
+				tasks: ['concat']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-myth');
 
 	grunt.registerTask('lint', ['jshint']);
-	grunt.registerTask('build', ['cssmin']);
-	grunt.registerTask('default', ['cssmin', 'jshint']);
+	grunt.registerTask('default', ['myth', 'cssmin', 'jshint', 'concat', 'uglify']);
 };
