eaa18b039ac09d07fcef65d03f3fb8dd5bdca62c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-03-17 20:22:33 +0200

    fix blink

--- public/js/onLoad.js
+++ public/js/onLoad.js
@@ -3,10 +3,6 @@
 
 	window.ls = window.ls || {};
 
-	if (window.location.hash === '#_=_') {
-		window.location.href = window.location.href.split('#')[0];
-	}
-
 	ls.getCookie = function (name) {
 		var units = document.cookie.split(name + '=');
 		if (units.length === 2) {
